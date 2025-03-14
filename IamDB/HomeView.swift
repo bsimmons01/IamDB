3eb7cc00d4ef63996edeaa9eee0dafd33a0f873c
Brian Simmons <bsimmons01@gmail.com>
2025-03-14 15:00:38 -0700

    HomeView screen designed.

---
+++ IamDB/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  IamDB
+//
+//  Created by Brian Simmons on 3/14/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var heroTestTitle = Constants.testTitleURL
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                LazyVStack {
+                    AsyncImage(url: URL(string: heroTestTitle)) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .overlay {
+                                LinearGradient(
+                                    stops: [Gradient.Stop(color: .clear, location: 0.8),
+                                            Gradient.Stop(color: .gradient, location: 1)],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            }
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height * 0.85)
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Text(Constants.playString)
+                                .ghostButton()
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Text(Constants.downloadString)
+                                .ghostButton()
+                        }
+                    }
+                    
+                    HorizontalListView(header: Constants.trendingMovieString)
+                    HorizontalListView(header: Constants.trendingTVString)
+                    HorizontalListView(header: Constants.topRatedMovieString)
+                    HorizontalListView(header: Constants.topRatedTVString)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
