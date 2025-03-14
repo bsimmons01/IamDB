cb73bb9fab2e00f4aab245765b47eaea9228fa71
Brian Simmons <bsimmons01@gmail.com>
2025-03-14 13:27:08 -0700

    Initial Commit

---
+++ IamDB/IamDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IamDBApp.swift
+//  IamDB
+//
+//  Created by Brian Simmons on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct IamDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
