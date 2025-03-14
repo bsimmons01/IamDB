379362475db6162f6ef68d482633752a95aa387e
Brian Simmons <bsimmons01@gmail.com>
2025-03-14 13:51:46 -0700

    String Constants file

---
+++ IamDB/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  IamDB
+//
+//  Created by Brian Simmons on 3/14/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+}
